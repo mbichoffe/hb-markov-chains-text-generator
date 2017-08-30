4f10c710a2fbd19c7d9664425cf4fa3422a4cc37
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-30 12:27:15 -0700

    Refactored and added feature to open and read multiple files

--- markov.py
+++ markov.py
@@ -4,17 +4,18 @@ from random import choice
 import sys
 
 
-def open_and_read_file(file_path):
+def open_and_read_file(*file_paths):
     """Take file path as string; return text as string.
 
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
+    text = ''
 
-    # your code goes here
-    f = open(file_path)
-
-    text = f.read()
+    for file_path in file_paths[0]:
+        f = open(file_path)
+        text += f.read()
+        f.close()
 
     return text
 
@@ -47,40 +48,40 @@ def make_chains(text_string, n):
     chains = {}
 
     words = text_string.split()
-    # for i in range(len(words) - 2):
-    #     key = (words[i], words[i+1])
-    #     if key not in chains:
-    #         chains[(words[i], words[i+1])] = []
 
-    #     chains[key].append(words[i+2])
+    if n > len(words):
+        print 'Length of n cannot be greater than text string length.'
+        sys.exit()
 
-    #
-
-    # return chains
-
-    # try using D.setdefault(K, [default])
-
-    # for i in range(len(words) - 2):
-
-
-    #     key = (words[i], words[i+1])
-    #     chains.setdefault(key, []).append(words[i+2])
-
-    # return chains
     for i in range(len(words) - n):
-        key = []
 
-        for j in range(i, i + n):
+        key = tuple(words[i: n + i])
 
-            key.append(words[j])
+        value = words[i + n]
 
-        t_key = tuple(key)
-        chains.setdefault(t_key, []).append(words[i + n])
+        chains.setdefault(key, []).append(value)
 
     return chains
 
 
-def make_text(chains):
+def make_first_link(chains):
+
+    first_link = []
+
+    caps_key = filter(lambda x: x[0][0].isupper(), chains.keys())
+
+    start_link = choice(caps_key)
+
+    [first_link.append(word) for word in start_link]
+
+    next_word = choice(chains[start_link])
+
+    first_link.append(next_word)
+
+    return first_link
+
+
+def make_text(chains, n):
     """Return text from chains."""
     # get random key to start the link
         # append into words list
@@ -88,37 +89,36 @@ def make_text(chains):
             # get random word within the list
             # append into words list
         # evaluate last two words in words[] to find key in dictionary
-    words = []
 
-    start_link = choice(chains.keys())
-
-    [words.append(word) for word in start_link]
-
-    third_word = choice(chains[start_link])
-
-    words.append(third_word)
+    markov_sentence = make_first_link(chains)
 
     while True:
-        last_two_words = (words[-2], words[-1])
-        if last_two_words not in chains:
+        last_n_words = tuple(markov_sentence[-n:])
+        if last_n_words not in chains:
             break
 
-        words.append(choice(chains[last_two_words]))
+        last_word = choice(chains[last_n_words])
 
-    return " ".join(words)
+        markov_sentence.append(last_word)
+
+        if last_word[-1] in '.!?"':
+            break
+
+    return " ".join(markov_sentence)
 
 
-input_path = sys.argv[1]
+input_path = sys.argv[1:]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 # print input_text
 
 # Get a Markov chain
-chains = make_chains(input_text, 4)
-print chains
+n = 3
+chains = make_chains(input_text, n)
+
 
 # Produce random text
-random_text = make_text(chains)
+random_text = make_text(chains, n)
 
 print random_text
